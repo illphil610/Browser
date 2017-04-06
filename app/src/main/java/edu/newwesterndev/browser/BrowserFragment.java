964f85e60761f4697d5f74b4d080e24c347a6f77
illphil610 <phil.cappelli@gmail.com>
2017-04-05 22:33:23 -0400

    First commit with moderate functionality

---
+++ app/src/main/java/edu/newwesterndev/browser/BrowserFragment.java
@@ -0,0 +1,96 @@
+package edu.newwesterndev.browser;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Toast;
+
+public class BrowserFragment extends Fragment {
+
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "Position";
+    private static final String ARG_PARAM2 = "urlKey";
+
+    private int position;
+    private String url;
+
+    //private OnFragmentInteractionListener mListener;
+
+    public BrowserFragment() {
+        // Required empty public constructor
+    }
+
+    public static BrowserFragment newInstance(int position, String url) {
+        BrowserFragment fragment = new BrowserFragment();
+        Bundle args = new Bundle();
+        args.putInt(ARG_PARAM1, position);
+        args.putString(ARG_PARAM2, url);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            position = getArguments().getInt(ARG_PARAM1);
+            url = getArguments().getString(ARG_PARAM2);
+        } else {
+            Log.d("Arguments", "getArguments doesn't have any args");
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_browser, container, false);
+        WebView webView = (WebView) view.findViewById(R.id.webViewFragment);
+
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setWebViewClient(new WebViewClient(){
+
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+                Toast.makeText(getContext(), request.getUrl().toString(), Toast.LENGTH_SHORT).show();
+                view.loadUrl(request.getUrl().toString());
+                url = request.getUrl().toString();
+                return true;
+            }
+        });
+
+        if(this.url != null) {
+            webView.loadUrl(this.url);
+        } else {
+            webView.loadUrl("https://developer.android.com/");
+        }
+        return view;
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        outState.putString("url", url);
+        super.onSaveInstanceState(outState);
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+    }
+}
---
+++ app/src/main/java/edu/newwesterndev/browser/MainActivity.java
@@ -0,0 +1,157 @@
+package edu.newwesterndev.browser;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.WebView;
+import android.widget.Button;
+import android.widget.EditText;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    static final String HOME_PAGE = "https://developer.android.com/index.html";
+
+    Toolbar myToolbar;
+    EditText editText;
+    Button goButton;
+    ViewPager viewPager;
+    Uri data;
+    String currentUrl;
+    int currentFrag, maxFrag;
+    WebAdapter webAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+        myToolbar.showOverflowMenu();
+
+        data = getIntent().getData();
+
+        currentFrag = 1;
+        maxFrag = 1;
+
+        if(data != null) {
+            currentUrl = data.toString();
+        } else {
+            currentUrl = HOME_PAGE;
+        }
+
+        goButton = (Button) findViewById(R.id.urlButton);
+        editText = (EditText) findViewById(R.id.urlEditText);
+        viewPager = (ViewPager) findViewById(R.id.viewPager);
+
+        webAdapter = new WebAdapter(getSupportFragmentManager());
+        viewPager.setAdapter(webAdapter);
+        webAdapter.addUrl(currentUrl);
+
+        viewPager.setCurrentItem(currentFrag);
+
+        goButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                currentUrl = editText.getText().toString();
+                WebView webView = (WebView) findViewById(R.id.webViewFragment);
+                webView.loadUrl(currentUrl);
+
+                webAdapter.updateUrl(currentUrl, viewPager.getCurrentItem());
+                webAdapter.notifyDataSetChanged();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.action_bar_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_back:
+                if (currentFrag > 0) {
+                    currentFrag--;
+                    viewPager.setCurrentItem(currentFrag);
+                    Log.d("Fragment Number", "Opening Fragment: " + currentFrag);
+                }
+                return true;
+            case R.id.action_forward:
+                if (currentFrag < maxFrag - 1) {
+                    currentFrag++;
+                    viewPager.setCurrentItem(currentFrag);
+                    Log.d("Fragment Number", "Opening Fragment: " + currentFrag);
+                }
+                return true;
+            case R.id.action_new:
+                maxFrag++;
+                currentFrag = maxFrag;
+                webAdapter.addUrl(editText.getText().toString());
+                webAdapter.notifyDataSetChanged();
+                viewPager.setCurrentItem(currentFrag);
+                Log.d("URL List", webAdapter.urls.toString());
+
+                // Move back a page because adapter always add two fragments
+                currentFrag--;
+                viewPager.setCurrentItem(currentFrag);
+
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    public class WebAdapter extends FragmentStatePagerAdapter {
+
+        ArrayList urls = new ArrayList<>();
+
+        public WebAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        public void addUrl(String str) {
+            urls.add(str);
+        }
+
+        public void updateUrl(String str, int position) {
+            urls.set(position, str);
+        }
+
+        @Override
+        public void startUpdate(ViewGroup container) {
+            super.startUpdate(container);
+        }
+
+        @Override
+        public int getCount() {
+            return maxFrag;
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            Log.d("getItem", String.valueOf(position));
+            return BrowserFragment.newInstance(position, urls.get(position).toString());
+        }
+
+        @Override
+        public int getItemPosition(Object object) {
+            return POSITION_NONE;
+        }
+
+    }
+}
